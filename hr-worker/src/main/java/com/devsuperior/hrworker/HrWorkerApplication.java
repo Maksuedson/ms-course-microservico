c32cd681742c00e5b9ca1a9a7a65c3b32efddecd
Maksuedson <maksuedson@hotmail.com>
2020-11-15 20:43:31 -0300

    wr-worker implementation

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -9,5 +9,7 @@ public class HrWorkerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrWorkerApplication.class, args);
 	}
-
+//	insert into tb_worker (name, daily_Income) values ('Bob', 200.0);
+//	insert into tb_worker (name, daily_Income) values ('Maria', 300.0);
+//	insert into tb_worker (name, daily_Income) values ('Alex', 250.0);
 }
